2a222c4e89591665778bb95671585ea76d83daea
Carlo Duaqui <carloduaqui@outlook.com>
2015-03-25 16:44:12 -0500

    Output products to index view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,7 @@
 class ProductsController < ApplicationController
   def index
     @vault_lots = VaultLot.all
-    @vault_name = VaultLot.status("")
   end
-
-
   # The associated view app/views/products/index.html.erb is auto-loaded
 
 end
